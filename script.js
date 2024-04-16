dc945416e4f7513933cbf940bb8e4adfa817d68c
Irfan Parwez <136509687+Iparwez-989@users.noreply.github.com>
2024-04-16 22:52:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const btn= document.getElementById('submit');btn.addEventListener('click',function () {
+// This url will open github portfolio in a new page
     window.open('https://github.com/Iparwez-989','_blank');  
   })
